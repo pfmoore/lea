5b55d5c1d669795ae24e88fdd3d0aca3c065d67c
pde <pde@n-side.com>
2014-12-19 22:18:02 +0100

    added new source files in setup.py

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.0.0-alpha.1"
+VER = "2.0.0-beta.1"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013, 2014  Pierre Denis
