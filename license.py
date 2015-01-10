63ecccc28b556e0fed27596184182a9ab8450b41
pde <pde@n-side.com>
2015-01-10 18:22:18 +0100

    version Lea 2.0.0-beta.5

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.0.0-beta.4"
+VER = "2.0.0-beta.5"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013, 2014, 2015 Pierre Denis
