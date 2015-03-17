4caf716ffad851a023354634ea9cfb8498c4f7b9
pde <pde@n-side.com>
2015-03-17 19:24:28 +0100

    version Lea 2.1.1

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.1.0"
+VER = "2.1.1"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013, 2014, 2015 Pierre Denis
