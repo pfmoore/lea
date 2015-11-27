05708308052c396c00066d798b966956c67d350f
Pierre Denis <pie.denis@skynet.be>
2015-11-28 00:22:23 +0100

    Version 2.2.0-beta.2 (+correct setup.py)

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.2.0-beta.1"
+VER = "2.2.0-beta.2"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013, 2014, 2015 Pierre Denis
