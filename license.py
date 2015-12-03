697415cbc20f626e65bc35ddbe9980c618db45cc
Pierre Denis <pie.denis@skynet.be>
2015-12-03 22:42:00 +0100

    Version 2.2.0-beta.3

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.2.0-beta.2"
+VER = "2.2.0-beta.3"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013, 2014, 2015 Pierre Denis
