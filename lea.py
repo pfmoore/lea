d4898c39285d2f927f7e7d5c4e09e9dd91b05852
Pierre Denis <pie.denis@skynet.be>
2015-12-17 01:00:27 +0100

    Correct Lea. sortBy method

--- lea.py
+++ lea.py
@@ -733,7 +733,7 @@ class Lea(object):
         # after prepending orderingLeas to self, the Alea returned by new() is sorted with orderingLeas;
         # then, extracting self (index -1) allows generating self's (v,p) pairs in the expected order;
         # these shall be used to create a new Alea, keeping the values in that order (no sort)
-        return Alea.fromValFreqsNoSort(*Lea.cprod(*orderingLeas).cprod(self).new()[-1]._genVPs())
+        return Alea.fromValFreqsOrdered(*Lea.cprod(*orderingLeas).cprod(self).new()[-1]._genVPs())
 
     def isAnyOf(self,*values):
         ''' returns a boolean probability distribution
