142a22405b0cca5f3eff0daeae74e53846afd2d2
Pierre Denis <pie.denis@skynet.be>
2016-02-28 21:39:18 +0100

    Add ordered argument in leaf.diceSeq, integrating the dice_unordered function of Paul Moore (refs #30)

--- leaf.py
+++ leaf.py
@@ -23,6 +23,9 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
+from itertools import combinations_with_replacement
+from math import factorial
+
 from lea.lea import *
 
 def die(nbFaces):
@@ -37,12 +40,50 @@ def dice(nbDice,nbFaces):
     '''
     return die(nbFaces).times(nbDice)
 
-def diceSeq(nbDice,nbFaces):
+def _genDiceSetFreqs(nbDice,nbFaces):
+    ''' generates tuple (outcome, weight)
+        where outcome is a tuple with nbDice elements giving individual results
+          obtained by throwing nbDice independent fair dice with faces marked
+          from 1 to nbFaces, ignoring the order
+        and weight is an integer giving the relative probability of this outcome
+        (from Paul Moore's idea and implementation)
+    '''
+    # The total number of permutations of N dice is N!
+    permutations = factorial(nbDice)
+    # Use itertools to get the sets.
+    for outcome in combinations_with_replacement(range(1,nbFaces+1),nbDice):
+        # For each set, the weight is N!/a!b!c!... where a, b, c... are
+        # the sizes of each group of equal values.
+        weight = permutations
+        # We run through the set counting an dividing as we go
+        runLen = 0
+        prevRoll = None
+        for roll in outcome:
+            if roll != prevRoll:
+                prevRoll = roll
+                runLen = 0
+            runLen = runLen + 1
+            if runLen > 1:
+                weight = weight // runLen
+        yield (outcome,weight)
+
+def diceSeq(nbDice,nbFaces,ordered=False):
     ''' returns an Alea instance representing the individual results obtained by
         throwing nbDice independent fair dice with faces marked from 1 to nbFaces
         (each value is a tuple with nbDice elements)
+        * if ordered is False (default), then the combinations of dice which are
+          the same apart from order are considered equal; the particular value
+          used is chosen to be in order from smallest to largest value
+        * if ordered is True, then all nbDice**nbFaces combinations are produced,
+          with equal probabilities
     '''
-    return die(nbFaces).timesTuple(nbDice)
+    if ordered:
+        return die(nbFaces).timesTuple(nbDice)
+    ## for ordered = False, note that the folliwng expression
+    ##    die(nbFaces).timesTuple(nbDice).map(lambda v: tuple(sorted(v))).getAlea()
+    ## gives same results but is ineffecient as nbDice and nbFaces grows
+    ## the fast function used here is due to Paul Moore
+    return Lea.fromValFreqs(*_genDiceSetFreqs(nbDice,nbFaces))
 
 # D6 represents the value obtained by throwing a fair die with 6 faces
 D6 = die(6)
