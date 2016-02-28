ad3b82bcf89967b5a1415df59ce309737360534a
Pierre Denis <pie.denis@skynet.be>
2016-02-28 20:12:53 +0100

    Version 2.2.0-beta.7

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "2.2.0-beta.6"
+VER = "2.2.0-beta.7"
 
 licenseText = '''\
 Lea %s Copyright (C) 2013-2016 Pierre Denis
