a8e09ea001f4c8d68f23cb6480cd1df53f22e079
piedenis <pie.denis@skynet.be>
2020-11-13 20:47:26 +0100

    version Lea 3.4.0

--- license.py
+++ license.py
@@ -23,7 +23,7 @@ along with Lea.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 '''
 
-VER = "3.3.2"
+VER = "3.4.0"
 
 license_text = '''\
 Lea %s Copyright (C) 2013-2020 Pierre Denis
